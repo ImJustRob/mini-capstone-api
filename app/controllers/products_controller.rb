c7a9f290606b299a89d1eef0ea73fc457f5e0dc6
ImJustRob <robertalothian@hotmail.com>
2024-09-01 18:03:39 -0400

    hardcoded create action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,13 +2,20 @@ class ProductsController < ApplicationController
     def index
         @products = Product.all
         render template: "products/index"
-      end
-    
-      def one
-        @product = Product.find_by(id: 1)    
-      def one    
-        @product = Product.find_by(id: params["id"])    
-        # show is for 1 product
-        render template: "products/show"
-      end
+    end
+
+        def show
+            @products = Product.find_by(id: params[:id])
+            render template: "products/show"
+        end
+            def create
+                @products = Product.new(
+                    name: "keyboard",
+                    price: 199,
+                    image_url: "https://media.wired.com/photos/6621af2c255c13bb362f5337/master/w_960,c_limit/logitech-pro-x-tkl-keyboard-Reviewer-Photo-SOURCE-Eric-Ravenscraft.jpg",
+                )
+                @products.save
+                render template: "products/show"
+            end
+
 end
