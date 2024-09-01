3fbc6401e2229a85387a69413f3cc4ea426efdf8
ImJustRob <robertalothian@hotmail.com>
2024-09-01 16:25:29 -0400

    updated actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,14 @@
 class ProductsController < ApplicationController
+    def index
+        @products = Product.all
+        render template: "products/index"
+      end
+    
+      def one
+        @product = Product.find_by(id: 1)    
+      def one    
+        @product = Product.find_by(id: params["id"])    
+        # show is for 1 product
+        render template: "products/show"
+      end
 end
