977547c2c7aaa6048d7dfe5ac6700b26dce3b4c8
ImJustRob <robertalothian@hotmail.com>
2024-09-01 18:15:31 -0400

    dynamic actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,11 +8,12 @@ class ProductsController < ApplicationController
             @products = Product.find_by(id: params[:id])
             render template: "products/show"
         end
+
             def create
                 @products = Product.new(
-                    name: "keyboard",
-                    price: 199,
-                    image_url: "https://media.wired.com/photos/6621af2c255c13bb362f5337/master/w_960,c_limit/logitech-pro-x-tkl-keyboard-Reviewer-Photo-SOURCE-Eric-Ravenscraft.jpg",
+                    name: params[:name],
+                    price: params[:price],
+                    image_url: params[:image_url]
                 )
                 @products.save
                 render template: "products/show"
